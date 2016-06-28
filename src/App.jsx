254151ffdb7457eaa83d994e1afbfcc6905d8472
Andrew William Seward <awswrd@gmail.com>
2016-06-28 00:07:07 -0700

    Fix some babel stuff

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,9 @@
 import React, { Component, PropTypes } from 'react';
 
 export default class App extends Component {
+  static propTypes = {};
+  static defaultProps = {};
+  state = {};
   render() {
     return (
       <div>
