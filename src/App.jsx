9fed7d7b1e4f9d46e7525eb53fe7660c4b331f72
Andrew William Seward <awswrd@gmail.com>
2016-06-27 23:54:12 -0700

    Start adding form

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,15 @@ import React, { Component, PropTypes } from 'react';
 
 export default class App extends Component {
   render() {
-    return <div>Hello</div>;
+    return (
+      <div>
+        <h3>
+          List of Things
+          <button onClick={() => {}}>
+            Refresh
+          </button>
+        </h3>
+      </div>
+    );
   }
 };
