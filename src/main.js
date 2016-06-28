9777a2186ffbb7b7aa6e84429c13edf015c4667e
Andrew William Seward <awswrd@gmail.com>
2016-06-27 23:49:51 -0700

    Move to an App component

--- src/main.js
+++ src/main.js
@@ -1,7 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+import App from './App.jsx';
+
 ReactDOM.render(
-  <div>Hello</div>,
+  <App />,
   document.getElementById('main-mount')
 );
